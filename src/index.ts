83ed920863c8101920570d6ba46d64f11dc29e9e
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-11-23 17:18:53 -0300

    agregando typescript

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ const app = express();
 //Routing
 
 app.get("/", (req, res) => {
-  res.send("Hola mundo en Express");
+  res.send("Hola mundo en Express / Typescript");
 });
 
 app.get("/ecommerce", (req, res) => {
   res.send("Este es el ecommerce");
 });
 
-const port =process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log("Servidor funcionando en el puerto:", port);
