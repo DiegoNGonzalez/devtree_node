5decb0d37d123dbe0d519cbff9d9133e689c5acc
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-11-23 16:55:29 -0300

    agregando nodemon y configurando server

---
+++ index.js
@@ -0,0 +1,19 @@
+//const express = require("express"); // CJS Common js
+import express from "express"; // ESM Ecmascript modules
+const app = express();
+
+//Routing
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo en Express");
+});
+
+app.get("/ecommerce", (req, res) => {
+  res.send("Este es el ecommerce");
+});
+
+const port =process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log("Servidor funcionando en el puerto:", port);
+});
