b39c7a85eedd9a8f72d594f05f32b34bfddf945e
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-11-24 13:29:42 -0300

    agregando conexion mongo atlas, y variables de entorno

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 //const express = require("express"); // CJS Common js
 import express from "express"; // ESM Ecmascript modules
+import 'dotenv/config'
 import router from "./router"
+import { connectDB } from "./config/db";
+
+
 const app = express();
 
+connectDB()
 
 //Leer datos de forms
 
