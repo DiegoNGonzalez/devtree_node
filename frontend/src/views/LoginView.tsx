fdb31f117a806494e42848530e7259862c03a4fe
Diego Gonzalez <90977032+DiegoNGonzalez@users.noreply.github.com>
2024-11-29 12:51:21 -0300

    agregando react router dom, login y register view

---
+++ frontend/src/views/LoginView.tsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+export default function LoginView() {
+  return (
+    <>
+    <div className="text-2xl">LoginView</div>
+    <nav>
+        <Link to="/auth/register">¿No tienes cuenta? Crea una aqui</Link>
+    </nav>
+    </>
+  )
+}
---
+++ frontend/src/views/RegisterView.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+
+export default function RegisterView() {
+  return (
+    <>
+    <div>RegisterView</div>
+    <nav>
+        <Link to="/auth/login">¿Ya tienes una cuenta? Inicia sesion aqui</Link>
+    </nav>
+    </>
+  )
+}
